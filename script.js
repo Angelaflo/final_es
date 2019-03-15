9841979effa77056be408061cda2af69bc05626d
Angelaflo <Angela_flores@davincischools.org>
2019-03-14 18:57:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-a = document.getElementById("correct1");
+a = document.getElementById("correct1"); // variables (a-f) that are linked to the tiles id in the html
 b = document.getElementById("correct2");
 c = document.getElementById("correct3");
 d = document.getElementById("correct4");
 e = document.getElementById("correct5");
 f = document.getElementById("correct6");
-g = document.getElementById("key");
+g = document.getElementById("key"); // variable of the key image
 
 function nextImage(el) {
 	if (el.src.match("images/button.png")){
 		 el.src = "images/cat.PNG";
 	} 
-}
+} // onclick feature when you click one of the correct tiles "button.png", it changes to the cat image
 
 function refreshPage(){
     window.location.reload();
-} 
+} // onclick feature that refreshes the page
 
 function checkImage(el){
 	if (a.src.match("images/cat.PNG") && b.src.match("images/cat.PNG") && c.src.match("images/cat.PNG")){
@@ -30,8 +30,8 @@ function checkImage(el){
 	else{
 		//i wish this function would work when all lights are yellow, or all are red...
 	}
-}
+} // onclick feature where you click coraline and checks if all the correct tiles and if it is, it makes the cat images opaque and makes the key that is hidden appear. If all the correct tiles isn't checked, this function won't do anyhthing.
 
 function nextPage(el) {
 	window.location.href = 'https://ashleygonzalez9.github.io/phase8/escape.html';
-}
+} //onclick feature where it takes you to the next page of the escape room. 
